3d09203a79cc7e45d42d4d7d1b2fcaa5afca3cf4
Louis Riehl <louisriehl@gmail.com>
2018-11-06 16:32:26 +0000

    finished ex2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+puts 'Access and update the first and second store'
+
+store1 = Store.find_by(id: 1)
+store2 = Store.find_by(id: 2)
+
+store1.name = 'Hudson Bay'
+store1.save
+
+stores = Store.all
+stores.each { |store| puts store.name }
